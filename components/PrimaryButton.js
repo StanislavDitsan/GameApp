5224e0e6c46fd37a058a420ff42d2c1ad7c119b6
StanislavDitsan <ditsanstas@gmail.com>
2024-04-02 11:57:19 +0200

    Applied styles to PrimaryButton

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,13 +1,41 @@
-import { View, Text } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 function PrimaryButton({ children }) {
-    return (
+    const [isPressed, setIsPressed] = useState(false);
 
-        <View View >
-            <Text>{children}</Text>
-        </View >
+    return (
+        <Pressable
+            onPressIn={() => setIsPressed(true)}
+            onPressOut={() => setIsPressed(false)}
+            style={({ pressed }) => [
+                styles.container,
+                pressed && { opacity: 0.75 }
+            ]}
+        >
+            <View>
+                <Text style={styles.buttonText}>{children}</Text>
+            </View>
+        </Pressable>
     );
 }
 
-
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#ffd036',
+        borderRadius: 28,
+        margin: 4,
+        padding: 8,
+        paddingHorizontal: 16,
+        shadowColor: 'black', //iOS
+        shadowOffset: { width: 2, height: 1 },
+        shadowRadius: 6,
+        shadowOpacity: 0.6, // iOS end
+    },
+    buttonText: {
+        textAlign: 'center',
+        fontSize: 18,
+    },
+});
